f108c6c67a2a558c290e9d90b431f1e5bf23af21
unknown <jyoungcode@gmail.com>
2019-05-18 22:28:48 +0900

    Updates

---
+++ precache-manifest.06a700e68e905da1eb7ae10e51087f81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b81d815d75f76e86497408a0e6fc227",
+    "url": "/testreact/index.html"
+  },
+  {
+    "revision": "add0a29bdac6ed59b2b8",
+    "url": "/testreact/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f7d040f465544c78d5f4",
+    "url": "/testreact/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "add0a29bdac6ed59b2b8",
+    "url": "/testreact/static/js/2.d7db6f3e.chunk.js"
+  },
+  {
+    "revision": "f7d040f465544c78d5f4",
+    "url": "/testreact/static/js/main.069d2f78.chunk.js"
+  },
+  {
+    "revision": "1d0bc4833f5a10c60360",
+    "url": "/testreact/static/js/runtime~main.30e0e851.js"
+  }
+]);
